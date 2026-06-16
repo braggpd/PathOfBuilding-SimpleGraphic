b87e6cbf776e45dac7cd2bbaff59e5f93e539f41
braggpd <patrick.d.bragg@gmail.com>
2026-06-16 13:56:07 -0400

    test: checking push works with real content

---
+++ ui_main.cpp
@@ -0,0 +1,18 @@
+// DyLua: SimpleGraphic
+// (c) David Gowor, 2014
+//
+// Module: UI Main
+//
+
+#include "ui_local.h"
+#if __APPLE__ && __MACH__
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <cstdio>
+#include <sys/mman.h>
+#endif
+
+// ======
+// Locals
+// ======
